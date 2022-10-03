c6cbc649a0e0a00a9de2dc11dbf81836015473b4
Gabriel Alejandro <gagp27pty@gmail.com>
2022-10-03 05:22:59 -0500

    database crm

--- mysql/crm-db/database-and-tables.sql
+++ mysql/crm-db/database-and-tables.sql
@@ -6,6 +6,7 @@ create table customers (
     id int(11) primary key auto_increment,
     name varchar(50) not null,
     email varchar(50) not null,
-    phone varchar(8) not null,
-    company varchar(50) not null
+    phone varchar(9) not null,
+    company varchar(50) not null,
+    image varchar(100)
 );
