ca897f6d7078aec9178ff8ad2d92bbde1d11a3af
Gabriel Alejandro <gagp27pty@gmail.com>
2023-01-09 00:15:18 -0500

    deleted no used

--- mysql/appsalon-db/views.sql
+++
@@ -1,6 +0,0 @@
-create view view_citas as
-    select citas.id, citas.fecha, citas.hora, concat(usuarios.nombre, " ", usuarios.apellido) as "cliente", usuarios.email, servicios.nombre as servicio, servicios.precio
-    from citas
-    inner join usuarios on citas.usuarioId = usuarios.id
-    inner join citas_x_servicios cxs on citas.id = cxs.citaId
-    inner join servicios on cxs.servicioId = servicios.id;
